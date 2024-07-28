ed2b1c29e0918fbbd04c3bdb14439bcbf5e6809c
Thandeka325 <marhwayiza325@gmail.com>
2024-07-28 09:02:51 -0700

    Project 0x11. C - printf

--- main.h
+++ main.h
@@ -5,13 +5,32 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <limits.h>
 
-/* Task 0 _printf function */
-int _printf(const char *format, ...);
+
+/**
+ * struct types_arguments - struct op
+ *
+ * @t: The operator
+ * @f: The function associated
+ */
+typedef struct types_arguments
+{
+	char t;
+	int (*f)(va_list va);
+} types;
+
 int _putchar(char c);
+int _printf(const char *format, ...);
+int t_char(va_list va);
+int t_string(va_list va);
+int print_number(va_list va);
 
-/* Function prototypes */
-int print_char(va_list args);
-int print_string(va_list args);
+
+
+
+
+
 
 #endif /* MAIN_H */
