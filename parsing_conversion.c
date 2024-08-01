13ea347d8afc3fe0942b6d742821aab18848389d
Tando Mjekula <tandomjekula@gmail.com>
2024-08-01 12:49:29 +0200

    update

--- parsing_conversion.c
+++ parsing_conversion.c
@@ -7,6 +7,25 @@
  * Return: The parsed flags.
  */
 int parse_flags(const char **format)
+	{
+    int flags = 0;
+
+    while (**format == '+' || **format == ' ' || **format == '#' || **format == '0' || **format == '-')
+    {
+        if (**format == '+')
+            flags |= 1 << 0;  /* Plus flag*/
+        else if (**format == ' ')
+            flags |= 1 << 1;  /* Space flag*/
+        else if (**format == '#')
+            flags |= 1 << 2;  /* Hash flag*/
+        else if (**format == '0')
+            flags |= 1 << 3;  /* Zero flag*/
+        else if (**format == '-')
+            flags |= 1 << 4;  /* Minus flag*/
+        (*format)++;
+    }
+    return flags;
+}
 
 /**
  * parse_width - Parses the width specifier
@@ -16,6 +35,24 @@ int parse_flags(const char **format)
  * Return: The parsed width.
  */
 int parse_width(const char **format, va_list args)
+	{
+    int width = 0;
+
+    if (**format == '*')
+    {
+        width = va_arg(args, int);
+        (*format)++;
+    }
+    else
+    {
+        while (**format >= '0' && **format <= '9')
+        {
+            width = width * 10 + (**format - '0');
+            (*format)++;
+        }
+    }
+    return width;
+}
 
 /**
  * parse_precision - Parses the precision specifier
@@ -25,6 +62,30 @@ int parse_width(const char **format, va_list args)
  * Return: The parsed precision.
  */
 int parse_precision(const char **format, va_list args)
+	int parse_precision(const char **format, va_list args)
+{
+    int precision = -1;
+
+    if (**format == '.')
+    {
+        (*format)++;
+        if (**format == '*')
+        {
+            precision = va_arg(args, int);
+            (*format)++;
+        }
+        else
+        {
+            precision = 0;
+            while (**format >= '0' && **format <= '9')
+            {
+                precision = precision * 10 + (**format - '0');
+                (*format)++;
+            }
+        }
+    }
+    return precision;
+}
 
 /**
  * parse_length - Parses the lenth modifier.
@@ -33,4 +94,19 @@ int parse_precision(const char **format, va_list args)
  * Return: The parsed length
  */
 int parse_length(const char **format)
+	{
+    int length = 0;
+
+    if (**format == 'l')
+    {
+        length = 1;
+        (*format)++;
+    }
+    else if (**format == 'h')
+    {
+        length = 2;
+        (*format)++;
+    }
+    return length;
+}
 
--- reversed.c
+++ reversed.c
@@ -7,3 +7,13 @@
  * Return: The number character printed.
  */
 int print_reversed(va_list args)
+	{
+    char *s = va_arg(args, char *);
+    int len = 0, i;
+
+    if (s == NULL)
+        s = "(null)";
+    for (i = strlen(s) - 1; i >= 0; i--)
+        len += _putchar(s[i]);
+    return (len);
+}
